fc9d5a043cfe257f42cebdc18bbe17dc7660fabd
nbailey04 <nathanbailey584@gmail.com>
2024-09-27 01:01:38 -0400

    Update Random Number Generator.asm

--- Random Number Generator.asm
+++ Random Number Generator.asm
@@ -67,3 +67,56 @@ main:
 
    mov  rax, 0
    ret
+;----------------------------------------
+
+; Assemble:       nasm -f elf64 lab12_pt3.asm
+; Link:           gcc lab12_pt3.o -o lab12_pt3
+
+    extern  srandom     ; We will use this external function
+    extern  random      ; We will use this external function
+    extern  puts        ; We will use this external function
+    extern  printf      ; We will use this external function
+    section .bss
+str: resb 101
+
+    section .data       ; Data section, initialized variables
+myseed:   dq 0          ; Variable to store the seed value
+newline:  db 10         ; Newline character
+
+    section .text
+    global main
+main:
+
+   ; Seed the random number generator
+   rdtsc                 ; Read the Time Stamp Counter
+   mov   [myseed], eax   ; Store the TSC value in myseed
+   mov   edi, eax        ; Set the seed value to EDI for srandom function
+   xor   rax, rax        ; Clear the A register
+   call  srandom         ; Seed the random number generator
+
+   xor rbx, rbx          ; Initialize B register to 0
+
+lp:
+   xor rax, rax                 ; Clear the A register
+   call random                  ; Generate a random number
+   and al, 0xF                  ; Limit values (0 to 15)
+   or al, 40h                   ; Check for '@'
+   mov byte [str + rbx], al     ; Store the random value in the str buffer
+   inc rbx                      ; Increment B register
+
+   cmp rbx, 100                 ; Compare the current value stored in B with 100
+   jl lp                        ; If less, repeat the loop
+
+   mov byte [str + rbx], 0     ; Null-terminate the string
+
+   ; Print the string
+   mov rdi, str
+   call puts
+
+   ; Print a newline
+   mov rdi, newline
+   mov rax, 0
+   call printf
+
+   mov rax, 0
+   ret
